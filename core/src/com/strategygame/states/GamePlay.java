f86f997dd8055429c4605b3f820e527367295a85
Pazuzu <carlosh555@hotmail.com>
2019-01-30 11:02:51 -0300

    Pushing tilemap changes

--- core/src/com/strategygame/states/GamePlay.java
+++ core/src/com/strategygame/states/GamePlay.java
@@ -1,14 +1,26 @@
 package com.strategygame.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.strategygame.handlers.GameStateManager;
 
 public class GamePlay extends GameState {
 
     BitmapFont font = new BitmapFont();
 
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer tiledMapRenderer;
+
     public GamePlay(GameStateManager gsm) {
         super(gsm);
+
+        tiledMap = new TmxMapLoader().load("tmx/forest.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
     }
 
     @Override
@@ -23,10 +35,10 @@ public class GamePlay extends GameState {
 
     @Override
     public void render() {
-        sb.setProjectionMatrix(camera.combined);
-        sb.begin();
-        font.draw(sb, "TESTE!", 100, 100);
-        sb.end();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
 
     }
 
